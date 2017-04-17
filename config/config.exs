cdbb3882175b06adab8895e3408ebd5d04933d38
Felipe Sere <felipesere@gmail.com>
2017-04-17 23:15:47 +0100

    Add a test watcher and nicer notifications

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+if Mix.env == :dev do
+  config :mix_test_watch, clear: true
+end
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
