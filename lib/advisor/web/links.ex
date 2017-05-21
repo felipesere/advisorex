64df6786bedd401f04fe424acbd443656c3ff59a
Felipe Sere <felipesere@gmail.com>
2017-05-21 15:21:07 +0100

    Remove nefarious tabs

--- lib/advisor/web/links.ex
+++ lib/advisor/web/links.ex
@@ -2,7 +2,7 @@ defmodule Advisor.Web.Links do
   def generate({:ok, %{advisories: advisories, questionnaire: questionnaire_id}}) do
     links = Enum.map(advisories, fn(advisory) ->
       %{link: "/provide/#{advisory.advice_id}", person: advisory.advisor}
-		end)
+    end)
     progress_link = "/progress/#{questionnaire_id}"
     {links, progress_link}
   end
