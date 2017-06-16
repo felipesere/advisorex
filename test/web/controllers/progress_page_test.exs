8d03b26d28d9849c0d8b88ada696a9809d0c1deb
Felipe Sere <felipesere@gmail.com>
2017-06-16 14:59:52 +0100

    Streamline test to simplify adding a new one

--- test/web/controllers/progress_page_test.exs
+++ test/web/controllers/progress_page_test.exs
@@ -2,30 +2,36 @@ defmodule Advisor.Web.ProgressPageTest do
   use Advisor.Web.ConnCase
   alias Advisor.Web.QuestionnaireProposal, as: Proposal
   alias Advisor.Web.Links
-  alias Advisor.Core.{Creator, People}
+  alias Advisor.Core.Creator
+
+  @sample_questions [5, 6]
 
   test "shows the progress filling in the questionnaires", %{conn: conn} do
-    rabea = People.find_by(name: "Rabea Gleissner")
-    felipe = People.find_by(name: "Felipe Sere")
-    cj = People.find_by(name: "Chris Jordan")
-
-    proposal = %Proposal{group_lead: 1,
-                         requester: rabea.id,
-                         advisors: [felipe.id, cj.id],
-                         questions: [5, 6]}
+    proposal = Proposal.build(for: "Rabea Gleissner",
+                              advisors: ["Chris Jordan", "Priya Patil"],
+                              group_lead: "Felipe Sere",
+                              questions: @sample_questions)
 
     {_, progress_link} = proposal
                          |> Creator.create
                          |> Links.generate
 
-    conn = conn
-           |> assign(:user_id, felipe.id)
-           |> get(progress_link)
+    conn
+    |> login_as("Felipe Sere")
+    |> get(progress_link)
+    |> html_response(200)
+    |> has_requester("Rabea Gleissner")
+    |> has_advisors(["Chris Jordan", "Priya Patil"])
+  end
 
-    html = html_response(conn, 200)
+  def has_requester(html, requester_name) do
+    assert requester(html) =~ requester_name
+    html
+  end
 
-    assert requester(html) =~ "Rabea Gleissner"
-    assert advisors(html)  == ["Felipe Sere", "Chris Jordan"]
+  def has_advisors(html, advisors_names) do
+    assert advisors(html) == advisors_names
+    html
   end
 
   def requester(html) do
