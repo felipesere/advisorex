415dd0468baf8abd3a3c0543576558da2ab2efe2
Felipe Sere <felipesere@gmail.com>
2017-06-16 16:16:58 +0100

    Make the timestamp automatic on the DB side

---
+++ priv/repo/migrations/20170616151357_make_the_timestamp_automatic.exs
@@ -0,0 +1,15 @@
+defmodule Advisor.Repo.Migrations.MakeTheTimestampAutomatic do
+  use Ecto.Migration
+
+  def up do
+    alter table(:answers) do
+      modify :inserted_at, :utc_datetime, default: fragment("NOW()")
+    end
+  end
+
+  def down do
+    alter table(:answers) do
+      modify :inserted_at, :utc_datetime, default: nil
+    end
+  end
+end
