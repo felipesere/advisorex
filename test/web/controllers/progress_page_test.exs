cbfb97c9a2c3cf4ab8883b8f78b393d16677db28
Felipe Sere <felipesere@gmail.com>
2017-06-17 13:29:59 +0100

    Refine 'completion of questionnaires' checking for answers

--- test/web/controllers/progress_page_test.exs
+++ test/web/controllers/progress_page_test.exs
@@ -6,12 +6,16 @@ defmodule Advisor.Web.ProgressPageTest do
 
   @sample_questions [5, 6]
 
-  test "shows the progress filling in the questionnaires", %{conn: conn} do
+  setup do
     proposal = Proposal.build(for: "Rabea Gleissner",
                               advisors: ["Chris Jordan", "Priya Patil"],
                               group_lead: "Felipe Sere",
                               questions: @sample_questions)
+    [proposal: proposal]
+  end
 
+  test "shows the progress filling in the questionnaires", %{conn: conn,
+                                                             proposal: proposal} do
     {_, progress_page} = proposal
                          |> Creator.create
                          |> Links.generate
@@ -24,15 +28,12 @@ defmodule Advisor.Web.ProgressPageTest do
     |> has_advisors(["Chris Jordan", "Priya Patil"])
   end
 
-  test "shows that an advisors has completed the advice form", %{conn: conn} do
-    proposal = Proposal.build(for: "Rabea Gleissner",
-                              advisors: ["Chris Jordan"],
-                              group_lead: "Felipe Sere",
-                              questions: [1])
-
-    {[%{link: link}], progress_page} = proposal
-                                       |> Creator.create
-                                       |> Links.generate
+  test "shows that an advisors has completed the advice form", %{conn: conn,
+                                                                 proposal: proposal} do
+    proposal =  %{proposal | questions: [1]}
+    {[%{link: link} | _], progress_page} = proposal
+                                           |> Creator.create
+                                           |> Links.generate
 
     conn
     |> login_as("Chris Jordan")
@@ -43,6 +44,38 @@ defmodule Advisor.Web.ProgressPageTest do
     |> get(progress_page)
     |> html_response(200)
     |> has_completed_advice()
+    |> has_continue_button_with("Waiting for further responses")
+  end
+
+  test "all completed feedback", %{conn: conn, proposal: proposal} do
+    {[%{link: cj}, %{link: priya}], progress_page} = proposal
+                                                    |> Creator.create
+                                                    |> Links.generate
+
+    answers = ["1": "something", "2": "else"]
+
+    conn
+    |> login_as("Chris Jordan")
+    |> post(cj, answers)
+
+    conn
+    |> login_as("Priya Patil")
+    |> post(priya, answers)
+
+    conn
+    |> login_as("Felipe Sere")
+    |> get(progress_page)
+    |> html_response(200)
+    |> has_continue_button_with("We are good to go")
+  end
+
+  def has_continue_button_with(html, text) do
+    button = html
+             |> Floki.find(".button")
+             |> Floki.text
+
+    assert button =~ text
+    html
   end
 
   def has_requester(html, requester_name) do
@@ -56,6 +89,11 @@ defmodule Advisor.Web.ProgressPageTest do
   end
 
   def has_completed_advice(html) do
+    assert html
+           |> Floki.find(".completeness")
+           |> Enum.map(&Floki.text/1)
+           |> Enum.any?(&(&1 =~ "Completed"))
+
     html
   end
 
