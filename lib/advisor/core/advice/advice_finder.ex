235e903b8cf47a5dd6ee8525bddca6925b5cf01f
Felipe Sere <felipesere@gmail.com>
2017-06-25 20:10:26 +0100

    Make usage of queries consistent in AdviceFinder

--- lib/advisor/core/advice/advice_finder.ex
+++ lib/advisor/core/advice/advice_finder.ex
@@ -4,13 +4,13 @@ defmodule Advisor.Core.AdviceFinder do
   import Ecto.Query
 
   def all_for(id) do
-    query = from advice in Advice, where: advice.questionnaire_id == ^id
-    Repo.all(query)
+    Repo.all(from advice in Advice, where: advice.questionnaire_id == ^id)
   end
 
   def find(id) do
     Repo.get(Advice, id)
   end
+
   def find(advice_id, [from_advisor: %{id: advisor_id}]) do
     Repo.one(from a in Advice, where: a.id == ^advice_id and a.advisor_id == ^advisor_id)
   end
