a21aa769cb0c1956136ee8ff564b079b43d7f1ae
Felipe Sere <felipesere@gmail.com>
2017-07-06 19:01:54 +0100

    Move the data to createa a questinnaire into its own module

--- lib/advisor/core/questionnaire/questionnaire.ex
+++ lib/advisor/core/questionnaire/questionnaire.ex
@@ -1,7 +1,7 @@
 defmodule Advisor.Core.Questionnaire do
   use Ecto.Schema
   import Ecto.Query
-  alias Advisor.Core.{Advice, People, Questions}
+  alias Advisor.Core.Advice
   alias Advisor.Repo
   alias __MODULE__
 
@@ -12,16 +12,6 @@ defmodule Advisor.Core.Questionnaire do
     field :requester_id, :integer
     field :group_lead, :integer
   end
-
-  def form_data() do
-    everybody = People.everybody()
-    group_leads = Enum.filter(everybody, &who_is_a_group_lead/1)
-    questions = Questions.all()
-
-    {everybody, group_leads, questions}
-  end
-
-  defp who_is_a_group_lead(person), do: person.is_group_lead
 
   def for_group_lead(group_lead_id) do
     Repo.all(from q in Questionnaire, where: q.group_lead == ^group_lead_id)
--- lib/advisor/web/controllers/questionnaire_page.ex
+++ lib/advisor/web/controllers/questionnaire_page.ex
@@ -1,12 +1,12 @@
 defmodule Advisor.Web.QuestionnairePage do
   use Advisor.Web, :controller
-  alias Advisor.Core.Questionnaire
+  alias Advisor.Core.Questionnaire.Form, as: QuestionnaireForm
   alias Advisor.Web.Authentication.User
 
   plug  Advisor.Web.Authentication.Gatekeeper
 
   def index(conn, _params) do
-    {everybody, group_leads, questions} = Questionnaire.form_data()
+    {everybody, group_leads, questions} = QuestionnaireForm.data()
 
     render conn, "request.html", requester: User.of(conn),
       group_leads: group_leads,
