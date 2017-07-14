49905cc2e35698281231c6cf31f6feb4507b429b
Felipe Sere <felipesere@gmail.com>
2017-07-14 17:24:32 +0100

    Remove trailing white space

--- lib/mix/tasks/password.generate.ex
+++ lib/mix/tasks/password.generate.ex
@@ -6,7 +6,7 @@ defmodule Mix.Tasks.Password.Generate do
 
   def run(_) do
     password = Mix.Shell.IO.prompt("Enter password that will be hashed:") |> String.trim
-    
+
     Bcrypt.hashpwsalt(password) |> IO.puts
   end
 end
