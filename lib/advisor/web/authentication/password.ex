33ff88ecd7f7e8be808b604c3a100fc4ebb0bfda
Felipe Sere <felipesere@gmail.com>
2017-07-15 09:19:47 +0100

    Clarify the intent of the hashed_password

--- lib/advisor/web/authentication/password.ex
+++ lib/advisor/web/authentication/password.ex
@@ -1,20 +1,20 @@
 defmodule Advisor.Web.Authentication.Password do
   alias Comeonin.Bcrypt
   def matches?(password) do
-    [checker: checker, password: real_hash] = Application.get_env(:advisor, __MODULE__)
+    [checker: checker, password: hashed_password] = Application.get_env(:advisor, __MODULE__)
 
-    checker.matches?(password, real_hash)
+    checker.matches?(password, hashed_password)
   end
 
   defmodule HashedPassword do
-    def matches?(password, hash) do
-      Bcrypt.checkpw(password, hash)
+    def matches?(password, hashed_password) do
+      Bcrypt.checkpw(password, hashed_password)
     end
   end
 
   defmodule SimplePassword do
-    def matches?(password, hash) do
-      password == hash
+    def matches?(password, hashed_password) do
+      password == hashed_password
     end
   end
 end
