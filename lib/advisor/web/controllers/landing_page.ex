03c8ec1823f6a7f5f65ed9007e2edc770d2d3495
Felipe Sere <felipesere@gmail.com>
2017-07-16 11:59:36 +0100

    Simplify retrieving 'target' for redirect into a table

--- lib/advisor/web/controllers/landing_page.ex
+++ lib/advisor/web/controllers/landing_page.ex
@@ -2,14 +2,13 @@ defmodule Advisor.Web.LandingPage do
   use Advisor.Web, :controller
 
   def index(conn, _params) do
-    target = case conn.cookies["target"] do
-      nil  -> false
-      "deleted" -> false
-      x -> x
-    end
-
     render conn, "index.html", button: "Ask for advice",
-                               target: target,
+                               target: target(conn),
                                message: false
   end
+
+  def target(%Plug.Conn{} = conn), do: target(conn.cookies["target"])
+  def target(nil), do: false
+  def target("deleted"), do: false
+  def target(target) when is_binary(target), do: target
 end
