9b00c6217b4ac88382b05e975af642b1f5081c2c
Felipe Sere <felipesere@gmail.com>
2017-07-20 22:16:40 +0100

    Directly set password on Heroku if necessary

--- lib/mix/tasks/password.generate.ex
+++ lib/mix/tasks/password.generate.ex
@@ -7,6 +7,16 @@ defmodule Mix.Tasks.Password.Generate do
   def run(_) do
     password = Mix.Shell.IO.prompt("Enter password that will be hashed:") |> String.trim
 
-    Bcrypt.hashpwsalt(password) |> IO.puts
+    hash = Bcrypt.hashpwsalt(password)
+    case Mix.Shell.IO.prompt("Set on heroku? [Y|n]") |> String.trim do
+      answer when answer in ["Y", "y", ""] -> set_on_heroku(hash)
+      _ -> print_to_shell(hash)
+    end
+  end
+
+  def print_to_shell(hash), do: IO.puts(hash)
+
+  def set_on_heroku(hash) do
+    System.cmd("heroku", ["config:set", "PASSWORD=#{hash}"])
   end
 end
