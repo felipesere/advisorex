04ead64f9652af600ff8625445518d5531c91b91
Felipe Sere <felipesere@gmail.com>
2017-09-01 16:39:51 +0100

    Remove useless doc comments

--- lib/advisor/application.ex
+++ lib/advisor/application.ex
@@ -1,8 +1,6 @@
 defmodule Advisor.Application do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
 
@@ -11,8 +9,6 @@ defmodule Advisor.Application do
       supervisor(AdvisorWeb.Endpoint, []),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Advisor.Supervisor]
     Supervisor.start_link(children, opts)
   end
