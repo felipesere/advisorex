229e5341b64cabd25e232f3bce0ec808a0858fa0
Felipe Sere <felipesere@gmail.com>
2017-09-15 10:21:01 +0100

    Adds a tiny task to launch the postgres server

--- launch.sh
+++
@@ -1,1 +0,0 @@
-pg_ctl start -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log
---
+++ lib/mix/tasks/db.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.Db do
+  use Mix.Task
+
+  @shortdoc "Start the postgres instance"
+
+  @pg_directory "/usr/local/var/postgres"
+
+
+  def run(_) do
+    System.cmd("pg_ctl", ["start", "-D", @pg_directory, "-l", "#{@pg_directory}/server.log"])
+  end
+end
