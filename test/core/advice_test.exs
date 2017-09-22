bc29e335609020d49e6a103dbdeebbf27ecbd70d
Felipe Sere <felipesere@gmail.com>
2017-09-22 22:08:55 +0100

    Drastically simplify the tests checking the completeness of an advice request

--- test/core/advice_test.exs
+++ test/core/advice_test.exs
@@ -1,37 +1,14 @@
 defmodule Advisor.Core.AdviceTest do
-  alias Advisor.Test.Support.Proposal
+  use ExUnit.Case
+  alias Advisor.Core.{Advice, Answer}
 
-  use Advisor.DataCase
-  alias Advisor.Core.Advice
-  alias Advisor.Core.Questionnaire.Creator
-
-  @phrases [1, 2, 3]
-
-  # TODO: These tests look... aweful!
   test "can figure out if an advice has been answered fully" do
-    christoph = advised_by("Christoph Gockel", @phrases)
-
-    # this can not be right. but I'll wait for a failure to direct me...
-    ThroughTheCore.answer!(christoph, with: %{"1" => "foo", "2" => "bar", "3" => "batz"})
-    advice = Advice.find(christoph.id)
-    assert Advice.completed?(advice, length(@phrases))
+    advice = %Advice{answers: [%Answer{}, %Answer{}, %Answer{}]}
+    assert Advice.completed?(advice, 3)
   end
 
   test "can tell if advice has not been answered fully" do
-    christoph = advised_by("Christoph Gockel", @phrases)
-
-    ThroughTheCore.answer!(christoph, with: %{"1" => "foo"})
-    advice = Advice.find(christoph.id)
-    refute Advice.completed?(advice, length(@phrases))
-  end
-
-  def advised_by(person, questions) do
-    {:ok, %{advisories: [individual]}} = Proposal.basic()
-                                         |> Proposal.with_advisors([person])
-                                         |> Proposal.with_questions(questions)
-                                         |> Proposal.build()
-                                         |> Creator.create
-
-    individual
+    advice = %Advice{answers: [%Answer{}, %Answer{}]}
+    refute Advice.completed?(advice, 3)
   end
 end
