6e1f4df9acbf85c077b328af052b01a93d7453b7
Felipe Sere <felipesere@gmail.com>
2017-09-23 23:06:15 +0100

    Streamline and download function

--- lib/advisor/core/summary/summary.ex
+++ lib/advisor/core/summary/summary.ex
@@ -5,27 +5,31 @@ defmodule Advisor.Core.Summary do
   alias Advisor.Repo
   import Ecto.Query
 
-  # TODO: Larget. Query. Every. Can this be simplified?
   def for_download(id) do
-    query = from advice in Advice,
-               join: r in Person, on: [id: advice.requester_id],
-               join: a in Person, on: [id: advice.advisor_id],
-               join: answer in Answer, on: [advice_request_id: advice.id],
-               where: advice.questionnaire_id == ^id,
-               group_by: [a.email, answer.inserted_at, r.name],
-               select: {answer.inserted_at, a.email, r.name, fragment("array_agg(answer)")}
-
-    content = query
-              |> Repo.all()
-              |> Enum.map(fn({ts, adv, req, answers}) -> [ts, adv, req] ++ answers end)
-              # TODO: Querky ending...
-
-    questions = id
-                |> Questionnaire.questions()
-                |> Questions.load()
-                |> Questions.phrases()
-    header = ["timestamp", "advisor", "requester"] ++ questions
-
-    [header | content]
+    [header(id) | content(id)]
   end
+
+  defp content(id) do
+    query = from advice in Advice,
+      join: r in Person, on: [id: advice.requester_id],
+      join: a in Person, on: [id: advice.advisor_id],
+      join: answer in Answer, on: [advice_request_id: advice.id],
+      where: advice.questionnaire_id == ^id,
+      group_by: [a.email, answer.inserted_at, r.name],
+      select: {answer.inserted_at, a.email, r.name, fragment("array_agg(answer)")}
+
+    query
+    |> Repo.all()
+    |> Enum.map(&(&1 |> Tuple.to_list() |> List.flatten()))
+  end
+
+  defp header(id) do
+    id
+    |> Questionnaire.questions()
+    |> Questions.load()
+    |> Questions.phrases()
+    |> prepend(["timestamp", "advisor", "requester"])
+  end
+
+  defp prepend(head, tail), do: tail ++ head
 end
