979820c15a0e0df46bc93278a82f60a2c50c858b
Felipe Sere <felipesere@gmail.com>
2017-11-12 23:48:34 +0000

    Use Gravatar if no image is available

--- lib/advisor_web/views/questionnaire_page_view.ex
+++ lib/advisor_web/views/questionnaire_page_view.ex
@@ -1,4 +1,16 @@
 defmodule AdvisorWeb.QuestionnairePageView do
   use AdvisorWeb, :view
 
+  def image_url(person) do
+    if person.profile_image == "" do
+      "https://secure.gravatar.com/avatar/#{hash_email(person.email)}?s=250"
+    else
+      person.profile_image
+    end
+  end
+
+  defp hash_email(email) do
+    :crypto.hash(:md5, String.downcase(email))
+    |> Base.encode16(case: :lower)
+  end
 end
