60fbda9c36389b7a3473dcb7a7b44297f1cf03b5
Felipe Sere <felipesere@gmail.com>
2017-11-16 19:16:15 +0000

    Allow chaning group_lead and profile_image settings

--- lib/advisor/core/people/person.ex
+++ lib/advisor/core/people/person.ex
@@ -1,10 +1,15 @@
 defmodule Advisor.Core.Person do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "people" do
     field :name,          :string
     field :profile_image, :string,  default: ""
     field :is_group_lead, :boolean, default: false
     field :email,         :string
+  end
+
+  def changeset(person, params \\ %{}) do
+    cast(person,  params, [:name, :profile_image, :is_group_lead])
   end
 end
--- lib/advisor_web/controllers/dashboard_page.ex
+++ lib/advisor_web/controllers/dashboard_page.ex
@@ -1,6 +1,6 @@
 defmodule AdvisorWeb.DashboardPage do
   use AdvisorWeb, :controller
-  alias Advisor.Core. Dashboard
+  alias Advisor.Core.{Dashboard, Person, People}
   alias AdvisorWeb.Authentication.User
 
   plug AdvisorWeb.Authentication.Gatekeeper
@@ -14,6 +14,16 @@ defmodule AdvisorWeb.DashboardPage do
     render conn, "index.html", viewer: viewer,
                                group_lead_section: group_lead_section,
                                required_advice_section: my_required_advice,
-                               advice_for_me: advice_for_me
+                               advice_for_me: advice_for_me,
+                               user: Person.changeset(viewer)
+
+  end
+
+  def settings(conn, %{"person" => person}) do
+    User.of(conn)
+    |> Person.changeset(person)
+    |> People.update()
+
+    redirect(conn, to: "/dashboard")
   end
 end
