415753ae5e040f432c044b6dc611d870ebb21381
Felipe Sere <felipesere@gmail.com>
2017-11-25 16:53:50 +0000

    Change to use @parks emails in dev

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,15 +15,15 @@ alias Advisor.Repo
 if Mix.env == :dev do
   Repo.delete_all Person
 
-  Repo.insert!(%Person{name: "Leslie Knope", email: "leslie@example.com", is_group_lead: true, profile_image: "https://media1.popsugar-assets.com/files/thumbor/2ebfSQkPUKjxKfb5N3h2N-VIh7U/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2015/02/24/097/n/1922283/79e574c2_edit_img_image_845210_1424395524/i/Best-Leslie-Knope-GIFs.png"})
+  Repo.insert!(%Person{name: "Leslie Knope", email: "leslie@parks.com", is_group_lead: true, profile_image: "https://media1.popsugar-assets.com/files/thumbor/2ebfSQkPUKjxKfb5N3h2N-VIh7U/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2015/02/24/097/n/1922283/79e574c2_edit_img_image_845210_1424395524/i/Best-Leslie-Knope-GIFs.png"})
 
-  Repo.insert!(%Person{name: "Ron Swanson", email: "ron@example.com", is_group_lead: true, profile_image: "https://pbs.twimg.com/profile_images/1369149006/ronProfile_400x400.jpg"})
-  Repo.insert!(%Person{name: "April Ludgate", email: "april@example.com", is_group_lead: true, profile_image: "https://i.pinimg.com/736x/ba/5f/e0/ba5fe0400e23ffb459b4d078484e6610--aubrey-plaza-aubrey-oday.jpg"})
-  Repo.insert!(%Person{name: "Andy Dwyer", email: "andy@example.com", profile_image: "https://pbs.twimg.com/profile_images/618214848975929344/iYgT29Up.jpg"})
-  Repo.insert!(%Person{name: "Tom Haverford", email: "tom@example.com", profile_image: "http://cdn.pastemagazine.com/www/blogs/lists/tomhaverford.png?1360172963"})
-  Repo.insert!(%Person{name: "Ann Perkins", email: "ann@example.com", profile_image: "https://upload.wikimedia.org/wikipedia/en/3/33/Ann_Perkins.jpg"})
-  Repo.insert!(%Person{name: "Donna Meagle", email: "donna@example.com", profile_image: "http://vignette4.wikia.nocookie.net/parksandrecreation/images/5/59/Donna_2.jpg/revision/latest?cb=20111015210204"})
-  Repo.insert!(%Person{name: "Ben Wyatt", email: "ben@example.com", profile_image: "https://vignette.wikia.nocookie.net/parksandrecreation/images/0/0a/Ben.jpg/revision/latest/scale-to-width-down/350?cb=20110809181418"})
-  Repo.insert!(%Person{name: "Jerry Gergich", email: "jerry@example.com", profile_image: "http://www.blursto.com/inc/images/r/113.jpg"})
-  Repo.insert!(%Person{name: "Chris Traeger", email: "chris@example.com", is_group_lead: true, profile_image: "https://memegenerator.net/img/images/600x600/1993394/chris-traeger.jpg"})
+  Repo.insert!(%Person{name: "Ron Swanson", email: "ron@parks.com", is_group_lead: true, profile_image: "https://pbs.twimg.com/profile_images/1369149006/ronProfile_400x400.jpg"})
+  Repo.insert!(%Person{name: "April Ludgate", email: "april@parks.com", is_group_lead: true, profile_image: "https://i.pinimg.com/736x/ba/5f/e0/ba5fe0400e23ffb459b4d078484e6610--aubrey-plaza-aubrey-oday.jpg"})
+  Repo.insert!(%Person{name: "Andy Dwyer", email: "andy@parks.com", profile_image: "https://pbs.twimg.com/profile_images/618214848975929344/iYgT29Up.jpg"})
+  Repo.insert!(%Person{name: "Tom Haverford", email: "tom@parks.com", profile_image: "http://cdn.pastemagazine.com/www/blogs/lists/tomhaverford.png?1360172963"})
+  Repo.insert!(%Person{name: "Ann Perkins", email: "ann@parks.com", profile_image: "https://upload.wikimedia.org/wikipedia/en/3/33/Ann_Perkins.jpg"})
+  Repo.insert!(%Person{name: "Donna Meagle", email: "donna@parks.com", profile_image: "http://vignette4.wikia.nocookie.net/parksandrecreation/images/5/59/Donna_2.jpg/revision/latest?cb=20111015210204"})
+  Repo.insert!(%Person{name: "Ben Wyatt", email: "ben@parks.com", profile_image: "https://vignette.wikia.nocookie.net/parksandrecreation/images/0/0a/Ben.jpg/revision/latest/scale-to-width-down/350?cb=20110809181418"})
+  Repo.insert!(%Person{name: "Jerry Gergich", email: "jerry@parks.com", profile_image: "http://www.blursto.com/inc/images/r/113.jpg"})
+  Repo.insert!(%Person{name: "Chris Traeger", email: "chris@parks.com", is_group_lead: true, profile_image: "https://memegenerator.net/img/images/600x600/1993394/chris-traeger.jpg"})
 end
