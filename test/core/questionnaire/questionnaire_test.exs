657bc23e381ca0678dceae45da8c4839f5fd3436
Felipe Sere <felipesere@gmail.com>
2017-11-26 09:14:44 +0000

    Simplify questionnaire tests

--- test/core/questionnaire/questionnaire_test.exs
+++ test/core/questionnaire/questionnaire_test.exs
@@ -1,19 +1,14 @@
 defmodule Advisor.Core.QuestionnaireTest do
   use Advisor.DataCase
-  alias Advisor.Test.Support.{Users, Proposal}
+  alias Advisor.Test.Support.Sample
 
-  alias Advisor.Core.Questionnaire.Creator
   alias Advisor.Core.Questionnaire
 
   test "finds a group leads questinnaires" do
-    [felipe | _] = Users.with(["Felipe Sere", "Rabea Gleissner", "Priya Patil", "Chris Jordan", "Jim Suchy"])
+    questionnaire = Sample.questionnaire(group_lead: "Felipe Sere",
+                                         requester: "Rabea Gleissner",
+                                         advisors: ["Priya Patil"])
 
-    Proposal.basic()
-    |> Proposal.with_group_lead(felipe.name)
-    |> Proposal.build()
-    |> Creator.create()
-
-    [%{group_lead: group_lead}] = Questionnaire.all_for_group_lead(felipe.id)
-    assert group_lead == felipe.id
+    assert [questionnaire] == Questionnaire.all_for_group_lead(questionnaire.group_lead)
   end
 end
