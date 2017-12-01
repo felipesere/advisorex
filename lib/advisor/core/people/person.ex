93610c3873c6c82e2d323a098db3b3e853630ac6
Felipe Sere <felipesere@gmail.com>
2017-12-01 12:30:32 +0000

    Default to the empty profile picture on the database level

--- lib/advisor/core/people/person.ex
+++ lib/advisor/core/people/person.ex
@@ -4,7 +4,7 @@ defmodule Advisor.Core.Person do
 
   schema "people" do
     field :name,          :string
-    field :profile_image, :string,  default: ""
+    field :profile_image, :string,  default: "https://maxcdn.icons8.com/Share/icon/p1em/Users/user1600.png"
     field :is_group_lead, :boolean, default: false
     field :email,         :string
   end
--- lib/advisor_web/views/questionnaire_page_view.ex
+++ lib/advisor_web/views/questionnaire_page_view.ex
@@ -1,11 +1,4 @@
 defmodule AdvisorWeb.QuestionnairePageView do
   use AdvisorWeb, :view
 
-  def image_url(person) do
-    if person.profile_image == "" do
-      "https://maxcdn.icons8.com/Share/icon/p1em/Users//user1600.png"
-    else
-      person.profile_image
-    end
-  end
 end
