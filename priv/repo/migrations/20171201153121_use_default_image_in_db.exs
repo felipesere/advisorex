f34e1d9e9739a89c43c706143ce13e897351fb0f
Felipe Sere <felipesere@gmail.com>
2017-12-01 15:38:07 +0000

    Adds migration to use the default image if non is present

---
+++ priv/repo/migrations/20171201153121_use_default_image_in_db.exs
@@ -0,0 +1,14 @@
+defmodule Advisor.Repo.Migrations.UseDefaultImageInDb do
+  use Ecto.Migration
+  import Ecto.Query
+  alias Advisor.Repo
+
+  def up do
+    from(p in "people", where: [profile_image: ""], update: [set: [profile_image: "https://maxcdn.icons8.com/Share/icon/p1em/Users/user1600.png"]])
+    |> Repo.update_all([])
+  end
+
+  def down do
+    from(p in "people", where: [profile_image: 'https://maxcdn.icons8.com/Share/icon/p1em/Users/user1600.png'], update: [set: [profile_image: ""]])
+  end
+end
