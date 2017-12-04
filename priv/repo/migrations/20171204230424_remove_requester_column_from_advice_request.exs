f02f2ebc45196ab33bb2b609b4e89ede591eb091
Felipe Sere <felipesere@gmail.com>
2017-12-04 23:08:05 +0000

    Remove unused requester_id column on advice_requests

---
+++ priv/repo/migrations/20171204230424_remove_requester_column_from_advice_request.exs
@@ -0,0 +1,15 @@
+defmodule Advisor.Repo.Migrations.RemoveRequesterColumnFromAdviceRequest do
+  use Ecto.Migration
+
+  def up do
+   alter table("advice_requests") do
+     remove(:requester_id)
+   end
+  end
+
+  def down do
+    alter table("advice_requests") do
+      add :requester_id, :integer, [null: true, default: :nil]
+    end
+  end
+end
