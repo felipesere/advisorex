b7becbe44dd9ff597811cede6078d3c102db2e3e
Felipe Sere <felipesere@gmail.com>
2017-12-17 15:25:39 +0000

    Hit ESC to leave the dialog and deny scrolling the background

--- assets/js/dashboardModal.js
+++ assets/js/dashboardModal.js
@@ -2,11 +2,18 @@ module.exports = {
   bind: function(selector) {
     var toggle = function(e) {
       $(".modal").toggleClass("is-active");
+      $("body").toggleClass("is-active");
     };
 
     $(selector).click(toggle);
     $(".keep").click(toggle);
     $(".modal-background").click(toggle);
+
+    $(document).keyup(function(event) {
+      if( $(".is-active").length > 0 && event.keyCode == 27) {
+        toggle();
+      }
+    });
 
     $(".remove").click(function(e) {
       var questionnaireId = $(this).data("questionnaire");
