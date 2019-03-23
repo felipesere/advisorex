100c13b30e5e0a02114b85cf81583639970af9c5
Felipe Sere <felipesere@gmail.com>
2019-03-23 22:15:40 +0000

    Remove uglifyJS as we barely have any JS anyway

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
