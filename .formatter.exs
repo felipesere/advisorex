498da0c76a756e0632530793843d60c93a9be7ac
Felipe Sere <felipesere@gmail.com>
2019-03-23 09:55:45 +0000

    Add basic formatter

---
+++ .formatter.exs
@@ -0,0 +1,17 @@
+[
+  import_deps: [:phoenix],
+  inputs: [
+    "*.{ex,exs}",
+    "{config,lib,priv,test}/**/*.{ex,exs}"
+  ]
+  locals_without_parens: [
+    # Formatter tests
+    assert_format: 2,
+    assert_format: 3,
+    assert_same: 1,
+    assert_same: 2,
+
+    # Errors tests
+    assert_eval_raise: 3
+  ]
+]
