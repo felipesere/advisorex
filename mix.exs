0b58f418ecc112164c6f3ee10e77cf8b9b0d690a
Felipe Sere <felipesere@gmail.com>
2019-03-23 09:36:32 +0000

    Stop complaning about unnecessarily quoted atoms in mix

--- mix.exs
+++ mix.exs
@@ -63,9 +63,9 @@ defmodule Advisor.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"],
-     "ci": ["test", "credo --strict"],
-     "seed": ["run priv/repo/seeds.exs"]
+     test: ["ecto.create --quiet", "ecto.migrate", "test"],
+     ci: ["test", "credo --strict"],
+     seed: ["run priv/repo/seeds.exs"]
     ]
   end
 end
