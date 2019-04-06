a256b2f3fcfde959f556654725005f38230cd0c2
Felipe Sere <felipesere@gmail.com>
2019-04-06 11:23:54 +0100

    Use Jason with Uberauth

--- config/config.exs
+++ config/config.exs
@@ -40,7 +40,8 @@ config :ueberauth, Ueberauth,
 
 config :ueberauth, Ueberauth.Strategy.Google.OAuth,
   client_id: System.get_env("GOOGLE_CLIENT_ID"),
-  client_secret: System.get_env("GOOGLE_CLIENT_SECRET")
+  client_secret: System.get_env("GOOGLE_CLIENT_SECRET"),
+  json_library: Jason
 
 config :advisor, Advisor.Core.Notifications.Email.Mailer,
   adapter: Bamboo.SendgridAdapter,
