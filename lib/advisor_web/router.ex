40d10aa49ba1f0bd4d4988761d02ec5c6d1dd20a
Felipe Sere <felipesere@gmail.com>
2019-05-12 18:00:12 +0100

    Describe each route briefly

--- lib/advisor_web/router.ex
+++ lib/advisor_web/router.ex
@@ -15,26 +15,44 @@ defmodule AdvisorWeb.Router do
   end
 
   scope "/", AdvisorWeb do
-    # Use the default browser stack
     pipe_through :browser
 
+    # view the landing page
     get "/", LandingPage, :index
     get "/logout", AuthenticationController, :logout
 
+
+    # view the fhe form to create a questionnaire
     get "/request", QuestionnairePage, :index
+
+    # submit the questionnaire you want to create
     post "/request", AdviceRequestController, :create
+
+    # view the status of a questionnaire, who are we waiting for and who is done
     get "/progress/:id", ProgressPage, :index
 
+    # view the form for an advisor to fill out
     get "/provide/:id", ProvideAdviceController, :index
+
+    # save the advice given by an advisor
     post "/provide/:id", ProvideAdviceController, :create
 
+    # view the collected advice for a given questionnaire
     get "/present/:id", PresentPage, :index
+
+    # download the collected advice as a CSV
     get "/present/:id/download.csv", DownloadSummaryController, :export
+
+    # view your dashboard
     get "/dashboard", DashboardPage, :index
+
+    # change your settings
     post "/dashboard/settings", DashboardPage, :settings
 
+    # delete a given questionnaire
     get "/questionnaire/:id/delete", QuestionnaireController, :delete
 
+    # keep loadbalancers happy
     get "/healthcheck", HealthcheckController, :index
   end
 
