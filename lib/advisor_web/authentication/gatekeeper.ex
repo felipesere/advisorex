0792d747ebe315e66d4a6d0202596fcd749e8d9c
Felipe Sere <felipesere@gmail.com>
2019-06-29 22:54:05 +0100

    Move admin authentication to gatekeeper

--- lib/advisor_web/authentication/gatekeeper.ex
+++ lib/advisor_web/authentication/gatekeeper.ex
@@ -3,11 +3,24 @@ defmodule AdvisorWeb.Authentication.Gatekeeper do
   alias Advisor.{Person, People}
   import Phoenix.Controller, only: [redirect: 2]
 
+
   def init(opts) do
     allowed_role = Keyword.get(opts, :only, :regular)
     redirect = Keyword.get(opts, :redirect, "/")
 
-    %{only: allowed_role, redirect: redirect}
+    token = :advisor
+            |> Application.get_env(AdvisorWeb.AdminController, [])
+            |> Keyword.fetch!(:api_key)
+
+    %{only: allowed_role, redirect: redirect, token: token}
+  end
+
+  def call(conn, %{only: :admin, token: token}) do
+    if authenticated?(conn, token) do
+      conn
+    else
+      unauthorized(conn)
+    end
   end
 
   def call(conn, opts) do
@@ -54,5 +67,23 @@ defmodule AdvisorWeb.Authentication.Gatekeeper do
   defp preserve_original_destination(conn) do
     conn
     |> put_session(:target, conn.request_path)
+  end
+
+  def authenticated?(conn, access_token) do
+    token =
+      conn
+      |> get_req_header("authorization")
+      |> parse()
+
+    token == access_token
+  end
+
+  def parse(["Bearer " <> token]), do: token
+  def parse(_), do: :unauthorized
+
+  def unauthorized(conn) do
+    conn
+    |> send_resp(401, "Unauthorized")
+    |> halt()
   end
 end
--- lib/advisor_web/controllers/admin_controller.ex
+++ lib/advisor_web/controllers/admin_controller.ex
@@ -1,20 +1,9 @@
 defmodule AdvisorWeb.AdminController do
   use AdvisorWeb, :controller
-
-  @access_token :advisor
-                |> Application.get_env(AdvisorWeb.AdminController, [])
-                |> Keyword.fetch!(:api_key)
+  plug AdvisorWeb.Authentication.Gatekeeper, only: :admin
 
   def create_person(conn, params) do
-    if authenticated?(conn) do
-      create(conn, params)
-    else
-      unauthorized(conn)
-    end
-  end
-
-  def unauthorized(conn) do
-    send_resp(conn, 401, "Unauthorized")
+    create(conn, params)
   end
 
   def create(conn, params) do
@@ -35,23 +24,7 @@ defmodule AdvisorWeb.AdminController do
   defp field_and_reason({field, {reason, _}}), do: {field, reason}
 
   def remove_person(conn, %{"email" => email}) do
-    if authenticated?(conn) do
-      Advisor.People.delete(email: email)
-      send_resp(conn, 200, "")
-    else
-      unauthorized(conn)
-    end
+    Advisor.People.delete(email: email)
+    send_resp(conn, 200, "")
   end
-
-  def authenticated?(conn) do
-    token =
-      conn
-      |> get_req_header("authorization")
-      |> parse()
-
-    token == @access_token
-  end
-
-  def parse(["Bearer " <> token]), do: token
-  def parse(_), do: :unauthorized
 end
